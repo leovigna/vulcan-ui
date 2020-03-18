7cf67305014f34e232f6d5234b2b51ef11a2ac81
Leo Vigna <leo@joinsaturn.com>
2020-03-18 20:23:10 +0100

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
+    env: {
+        node: true
+    },
     parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
     extends: [
         'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
     ],
+    plugins: ['@typescript-eslint', 'prettier'],
     parserOptions: {
         ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
         sourceType: 'module',  // Allows for the use of imports
@@ -15,11 +19,36 @@ module.exports = {
     },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+        'semi': 0,
+        'react/jsx-filename-extension': [1, {
+            'extensions': ['.js', '.jsx', '.ts', '.tsx']
+        }],
+        'react/prop-types': 0,
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        'no-console': 'warn',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'warn', // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+        'no-empty': 'warn'
     },
     settings: {
         react: {
             version: 'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
         },
+        'import/resolver': {
+            'node': {
+                'extensions': ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
     },
+    'overrides': [
+        {
+            'files': ['**/*.ts', '**/*.tsx'],
+        }
+    ]
 };
