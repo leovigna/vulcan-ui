eabb371a97a8e9063a6fedcc2812f4dc5bacf58d
Leo Vigna <leo.vigna@gmail.com>
2020-05-26 14:36:35 +0200

    Storybook ts-config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    stories: ['../src/**/*.stories.js'],
+    stories: ['../src/**/*.stories.js', '../src/**/*.stories.tsx'],
     addons: [
         '@storybook/preset-create-react-app',
         '@storybook/addon-links',
@@ -16,7 +16,7 @@ module.exports = {
         // You can change the configuration based on that.
         // 'PRODUCTION' is used when building the static version of storybook.
 
-        // Make whatever fine-grained changes you need
+        //SCSS
         config.module.rules.push({
             test: /\.scss$/,
             use: [
@@ -36,6 +36,29 @@ module.exports = {
             include: path.resolve(__dirname, '../'),
         });
 
+        //TSX
+        config.module.rules.push({
+            test: /\.(ts|tsx)$/,
+            loader: require.resolve('babel-loader'),
+            options: {
+                presets: [['react-app', { flow: false, typescript: true }]],
+            },
+        })
+        /*
+        config.module.rules.push({
+            test: /\.(ts|tsx)$/,
+            use: [
+                {
+                    loader: require.resolve('ts-loader'),
+                },
+                // Optional
+                {
+                    loader: require.resolve('react-docgen-typescript-loader'),
+                },
+            ],
+        });
+        */
+        config.resolve.extensions.push('.ts', '.tsx');
 
         // Return the altered config
         return config;
