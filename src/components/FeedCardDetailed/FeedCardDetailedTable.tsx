8fb90e1e879439f9373d57a71f7cadadb74db024
Leo Vigna <leo@vulcan.link>
2020-07-27 21:36:50 +0200

    HomeView show TableView

---
+++ src/components/FeedCardDetailed/FeedCardDetailedTable.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import { CButton as Button } from '@coreui/react';
+import { FeedTypes } from '../../store/types';
+import { renderAnswer } from '../../store/feed/actions';
+import moment from 'moment';
+import HeartFilled from '../../assets/img/icons/heart_filled.svg';
+import HeartEmpty from '../../assets/img/icons/heart_empty.svg';
+
+interface Props {
+    feeds: FeedTypes.Feed[];
+    setContractFavorite: any;
+}
+
+const FeedCardDetailedTable = ({ feeds, setContractFavorite }: Props) => {
+    return (
+        <table
+            className="table table-outline mb-0 d-sm-table table-responsive table-borderless"
+            style={{ borderCollapse: 'collapse', border: 'none', borderSpacing: 0 }}
+        >
+            <thead>
+                <tr>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', minWidth: 50 }}
+                    >
+                        Protocol
+                    </th>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', minWidth: 100 }}
+                    >
+                        Name
+                    </th>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', maxWidth: 400 }}
+                    >
+                        Address
+                    </th>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', minWidth: 100 }}
+                    >
+                        Value
+                    </th>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', minWidth: 100 }}
+                    >
+                        Last Update
+                    </th>
+                    <th
+                        className="px-1 py-3"
+                        style={{ border: 'none', fontSize: 16, fontWeight: 'bold', color: '#FA4706', maxWidth: 50 }}
+                    >
+                        Favorite
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {feeds.map(
+                    ({
+                        id,
+                        name,
+                        protocol,
+                        protocolInfo,
+                        address,
+                        networkId,
+                        favorite,
+                        state,
+                        answerRenderOptions,
+                    }) => {
+                        const url = `#/feeds/${protocol}/${name}`;
+                        const protocolUrl = `#/protocols/${protocol}`;
+                        const hearted = favorite?.favorite || false;
+                        const protocolImg = protocolInfo?.img || '';
+                        const valueRender = state?.value ? renderAnswer(answerRenderOptions!, state!.value) : '';
+                        const timestampRender = state?.timestamp
+                            ? moment(state!.timestamp, 'X').format('MMMM D - h:mm A')
+                            : '';
+
+                        return (
+                            <tr key={id}>
+                                <td className="px-1 py-3">
+                                    <a href={protocolUrl}>
+                                        <img
+                                            style={{ width: 37, objectFit: 'contain' }}
+                                            src={protocolImg}
+                                            alt={protocol}
+                                        />
+                                    </a>
+                                </td>
+                                <td className="px-1 py-3">
+                                    <a href={url}>{name}</a>
+                                </td>
+                                <td className="px-1 py-3" style={{ fontSize: 15, fontWeight: 300, color: '#000000' }}>
+                                    <a
+                                        style={{ color: '#828282', fontStyle: 'italic' }}
+                                        href={`https://etherscan.com/address/${address}`}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        {address}
+                                    </a>
+                                </td>
+                                <td className="px-1 py-3" style={{ fontSize: 15, fontWeight: 300, color: '#000000' }}>
+                                    {valueRender}
+                                </td>
+                                <td className="px-1 py-3" style={{ fontSize: 15, fontWeight: 300, color: '#000000' }}>
+                                    {timestampRender}
+                                </td>
+                                <td className="px-1 py-3">
+                                    <Button
+                                        onClick={() =>
+                                            setContractFavorite({ id, address, networkId, favorite: !hearted })
+                                        }
+                                        style={{ padding: 0 }}
+                                    >
+                                        <img
+                                            style={{ width: 31, objectFit: 'contain' }}
+                                            src={hearted ? HeartFilled : HeartEmpty}
+                                            alt=""
+                                        ></img>
+                                    </Button>
+                                </td>
+                            </tr>
+                        );
+                    },
+                )}
+            </tbody>
+        </table>
+    );
+};
+
+export default FeedCardDetailedTable;
--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,86 +1,94 @@
 import React, { useState } from 'react';
-import {
-    CRow as Row,
-    CCol as Col,
-    CContainer as Container,
-    CButton as Button
-} from '@coreui/react'
+import { CRow as Row, CCol as Col, CContainer as Container, CButton as Button } from '@coreui/react';
 
-import ProtocolCard from '../../components/ProtocolCard'
-import FeedCardDetailedGrid from '../../components/FeedCardDetailed/FeedCardDetailedGrid'
+import ProtocolCard from '../../components/ProtocolCard';
+import FeedCardDetailedGrid from '../../components/FeedCardDetailed/FeedCardDetailedGrid';
 import { Feed } from '../../store/feed/types';
 import { Protocol } from '../../store/protocol/types';
+import FeedCardDetailedTable from '../../components/FeedCardDetailed/FeedCardDetailedTable';
 
 interface Props {
-    feeds: Feed[],
-    favoriteFeeds: Feed[]
-    protocols: Protocol[]
-    setContractFavorite: any
+    feeds: Feed[];
+    favoriteFeeds: Feed[];
+    protocols: Protocol[];
+    setContractFavorite: any;
 }
 
 const HomeView = ({ feeds, favoriteFeeds, protocols, setContractFavorite }: Props) => {
-    const [minimizeFeeds, setMinimizeFeeds] = useState(true);
     const [minimizeFavoriteFeeds, setMinimizeFavoriteFeeds] = useState(true);
 
-    const toggleMinimizeFeeds = () => setMinimizeFeeds(!minimizeFeeds);
     const toggleMinimizeFavoriteFeeds = () => setMinimizeFavoriteFeeds(!minimizeFavoriteFeeds);
 
-    const feedsMinimizeCount = 9;
-    const favoriteFeedsMinimizeCount = 3
-    const displayedFeeds = minimizeFeeds ? feeds.slice(0, feedsMinimizeCount) : feeds
-    const displayedFavoriteFeeds = minimizeFavoriteFeeds ? favoriteFeeds.slice(0, favoriteFeedsMinimizeCount) : favoriteFeeds
+    const favoriteFeedsMinimizeCount = 3;
+    const displayedFavoriteFeeds = minimizeFavoriteFeeds
+        ? favoriteFeeds.slice(0, favoriteFeedsMinimizeCount)
+        : favoriteFeeds;
 
     return (
         <div style={{ marginTop: -200, paddingTop: 100, paddingBottom: 50, marginRight: -15, marginLeft: -15 }}>
-
             <Container>
                 <Row>
                     <Col xs={12}>
                         <h1 style={{ fontSize: 40, fontWeight: 'bold', color: '#393939', height: 95 }}>Protocols</h1>
                     </Col>
-                    {
-                        protocols.map(({ description, active, id, name, img, feedCount, nodeCount, sponsorCount }, idx) =>
+                    {protocols.map(
+                        ({ description, active, id, name, img, feedCount, nodeCount, sponsorCount }, idx) => (
                             <Col key={idx} lg="3" md="6" xs="12">
-                                <ProtocolCard href={`#/protocols/${id}`} active={active} name={name} description={description} img={img} feedCount={feedCount} nodeCount={nodeCount} sponsorCount={sponsorCount} />
-                            </Col>)
-                    }
+                                <ProtocolCard
+                                    href={`#/protocols/${id}`}
+                                    active={active}
+                                    name={name}
+                                    description={description}
+                                    img={img}
+                                    feedCount={feedCount}
+                                    nodeCount={nodeCount}
+                                    sponsorCount={sponsorCount}
+                                />
+                            </Col>
+                        ),
+                    )}
                 </Row>
                 <Row>
                     <Col xs={12}>
                         <h1 style={{ fontSize: 40, fontWeight: 'bold', color: '#393939' }}>Favorite Feeds</h1>
                     </Col>
                     <FeedCardDetailedGrid setContractFavorite={setContractFavorite} feeds={displayedFavoriteFeeds} />
-                    {favoriteFeeds.length > favoriteFeedsMinimizeCount ?
+                    {favoriteFeeds.length > favoriteFeedsMinimizeCount ? (
                         <Col xs={12}>
                             <div className="d-flex justify-content-center">
-                                <Button onClick={toggleMinimizeFavoriteFeeds} style={{ fontSize: 20, fontWeight: 'medium', color: '#002C69' }}>{minimizeFavoriteFeeds ? <>View All ({favoriteFeeds.length - favoriteFeedsMinimizeCount} more)</> : <>Hide</>}</Button>
+                                <Button
+                                    onClick={toggleMinimizeFavoriteFeeds}
+                                    style={{ fontSize: 20, fontWeight: 'medium', color: '#002C69' }}
+                                >
+                                    {minimizeFavoriteFeeds ? (
+                                        <>View All ({favoriteFeeds.length - favoriteFeedsMinimizeCount} more)</>
+                                    ) : (
+                                        <>Hide</>
+                                    )}
+                                </Button>
                             </div>
                         </Col>
-                        : ''
-                    }
+                    ) : (
+                        ''
+                    )}
                 </Row>
                 <Row>
                     <Col xs={12}>
                         <h1 style={{ fontSize: 40, fontWeight: 'bold', color: '#393939' }}>Feeds</h1>
                     </Col>
-                    <FeedCardDetailedGrid setContractFavorite={setContractFavorite} feeds={displayedFeeds} />
-                    {feeds.length > feedsMinimizeCount ?
-                        <Col xs={12}>
-                            <div className="d-flex justify-content-center">
-                                <Button onClick={toggleMinimizeFeeds} style={{ fontSize: 20, fontWeight: 'medium', color: '#002C69' }}>{minimizeFeeds ? <>View All ({feeds.length - feedsMinimizeCount} more)</> : <>Hide</>}</Button>
-                            </div>
-                        </Col>
-                        : ''}
+                    <Col xs="12">
+                        <FeedCardDetailedTable setContractFavorite={setContractFavorite} feeds={feeds} />
+                    </Col>
                 </Row>
-            </Container >
+            </Container>
         </div>
-    )
-}
+    );
+};
 
 HomeView.defaultProps = {
     feeds: [],
     favoriteFeeds: [],
-    protocols: []
-}
+    protocols: [],
+};
 
 export default HomeView;
