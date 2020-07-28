3ed5e5b16290608daec171e117c6525ff2042b5e
Leo Vigna <leo@vulcan.link>
2020-07-28 19:48:29 +0200

    Add Coinbase Feeds

--- src/data/feeds/coinbase.ts
+++ src/data/feeds/coinbase.ts
@@ -1,46 +1,198 @@
 //@ts-nocheck
 
-import { CoinbaseFeed } from "../../store/feed/types";
+import { CoinbaseFeed } from '../../store/feed/types';
 
 export const testcontracts: CoinbaseFeed[] = [
     {
-        id: "0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-btc",
-        networkId: "1",
-        protocol: "coinbase",
-        address: "0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC",
-        name: "btc-usd",
-        title: "BTC/USD",
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-btc',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'btc-usd',
+        title: 'BTC/USD',
         answerRenderOptions: {
             transform: {
                 multiply: 1,
-                decimals: 3
+                decimals: 3,
             },
-            format: "$ %(value)s"
+            format: '$ %(value)s',
         },
         symbol: 'BTC',
-        index: 0
-    }
-]
+        index: 0,
+    },
+];
 
 const contracts: CoinbaseFeed[] = [
     {
-        id: "0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-btc",
-        networkId: "1",
-        protocol: "coinbase",
-        address: "0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC",
-        name: "btc-usd",
-        title: "BTC/USD",
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-btc',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'btc-usd',
+        title: 'BTC/USD',
         answerRenderOptions: {
             transform: {
                 multiply: 1,
-                decimals: 3
+                decimals: 3,
             },
-            format: "$ %(value)s"
+            format: '$ %(value)s',
         },
         symbol: 'BTC',
-        index: 0
-    }
-
-]
+        index: 0,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-eth',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'eth-usd',
+        title: 'ETH/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'ETH',
+        index: 1,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-xtz',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'xtz-usd',
+        title: 'XTZ/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'XTZ',
+        index: 2,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-dai',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'dai-usd',
+        title: 'DAI/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'DAI',
+        index: 3,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-rep',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'rep-usd',
+        title: 'REP/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'REP',
+        index: 4,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-zrx',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'zrx-usd',
+        title: 'ZRX/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'ZRX',
+        index: 5,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-bat',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'bat-usd',
+        title: 'BAT/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'BAT',
+        index: 6,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-knc',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'knc-usd',
+        title: 'KNC/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'KNC',
+        index: 7,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-link',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'link-usd',
+        title: 'LINK/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'LINK',
+        index: 8,
+    },
+    {
+        id: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC-comp',
+        networkId: '1',
+        protocol: 'coinbase',
+        address: '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC',
+        name: 'comp-usd',
+        title: 'COMP/USD',
+        answerRenderOptions: {
+            transform: {
+                multiply: 1,
+                decimals: 3,
+            },
+            format: '$ %(value)s',
+        },
+        symbol: 'COMP',
+        index: 9,
+    },
+];
 
 export default contracts;
--- src/store/coinbase/types.ts
+++ src/store/coinbase/types.ts
@@ -1,38 +1,29 @@
-export type CoinbaseTicker =
-    'BTC'
-    | 'ETH'
-    | 'XTZ'
-    | 'DAI'
-    | 'REP'
-    | 'ZRX'
-    | 'BAT'
-    | 'KNC'
-    | 'LINK'
+export type CoinbaseTicker = 'BTC' | 'ETH' | 'XTZ' | 'DAI' | 'REP' | 'ZRX' | 'BAT' | 'KNC' | 'LINK' | 'COMP';
 
 export interface CoinbaseOracleResponse {
-    timestamp: string,
-    messages: string[9],
-    signatures: string[9],
+    timestamp: string;
+    messages: string[9];
+    signatures: string[9];
     prices: {
-        [ticker in CoinbaseTicker]: string
-    }
+        [ticker in CoinbaseTicker]: string;
+    };
 }
 
 export interface CoinbaseOracle {
-    timestamp: string,
-    message: string,
-    signature: string,
-    price: string
+    timestamp: string;
+    message: string;
+    signature: string;
+    price: string;
 }
 
-export const START_POLL_COINBASE_ORACLE = 'ORM/START_POLL_COINBASE_ORACLE'
-export const POLL_COINBASE_ORACLE_ERROR = 'ORM/POLL_COINBASE_ORACLE_ERROR'
-export const STOP_POLL_COINBASE_ORACLE = 'ORM/STOP_POLL_COINBASE_ORACLE'
-export const CREATE_COINBASE_ORACLE_RESPONSE = 'ORM/CREATE_COINBASE_ORACLE_RESPONSE'
-export type CreateCoinbaseOracleResponseActionInput = CoinbaseOracleResponse
+export const START_POLL_COINBASE_ORACLE = 'ORM/START_POLL_COINBASE_ORACLE';
+export const POLL_COINBASE_ORACLE_ERROR = 'ORM/POLL_COINBASE_ORACLE_ERROR';
+export const STOP_POLL_COINBASE_ORACLE = 'ORM/STOP_POLL_COINBASE_ORACLE';
+export const CREATE_COINBASE_ORACLE_RESPONSE = 'ORM/CREATE_COINBASE_ORACLE_RESPONSE';
+export type CreateCoinbaseOracleResponseActionInput = CoinbaseOracleResponse;
 export interface CreateCoinbaseOracleResponseAction {
-    type: typeof CREATE_COINBASE_ORACLE_RESPONSE
-    payload: CreateCoinbaseOracleResponseActionInput
+    type: typeof CREATE_COINBASE_ORACLE_RESPONSE;
+    payload: CreateCoinbaseOracleResponseActionInput;
 }
 
-export type CoinbaseAction = CreateCoinbaseOracleResponseAction
+export type CoinbaseAction = CreateCoinbaseOracleResponseAction;
