4594dee8c8a05fb85a1212997c4efda759b4e260
Leo Vigna <leo@vulcan.link>
2020-07-29 19:33:05 +0200

    Updates

---
+++ precache-manifest.43ef8d2dd41bba14b8235081cffecfe5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259fcec2afffd943035f794397312af4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84068aaf01a7943e7ed9",
+    "url": "./static/css/4.eb736864.chunk.css"
+  },
+  {
+    "revision": "284ca80cb8d781006c88",
+    "url": "./static/css/main.2062d0aa.chunk.css"
+  },
+  {
+    "revision": "eb60536b6300407e28e8",
+    "url": "./static/js/0.45606955.chunk.js"
+  },
+  {
+    "revision": "0af868dcee6c4d9ff0c7",
+    "url": "./static/js/1.087af6ea.chunk.js"
+  },
+  {
+    "revision": "53ff983cf51a6f956ac693abc1204732",
+    "url": "./static/js/1.087af6ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2de7ccd75513202934",
+    "url": "./static/js/10.b78a4e1a.chunk.js"
+  },
+  {
+    "revision": "84068aaf01a7943e7ed9",
+    "url": "./static/js/4.0ea1b370.chunk.js"
+  },
+  {
+    "revision": "b2c570b7d7a08af60733f0de755c1bd6",
+    "url": "./static/js/4.0ea1b370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670252536c70c4a2b4c7",
+    "url": "./static/js/5.e433d02e.chunk.js"
+  },
+  {
+    "revision": "7a6831e2731fee4018f1",
+    "url": "./static/js/6.4bcd277c.chunk.js"
+  },
+  {
+    "revision": "4efb5b26e75d04f97cec",
+    "url": "./static/js/7.b56752fc.chunk.js"
+  },
+  {
+    "revision": "2de98becd39a7d713a12",
+    "url": "./static/js/8.14d114b3.chunk.js"
+  },
+  {
+    "revision": "35b0d99acccb62ace222",
+    "url": "./static/js/9.fe2a9168.chunk.js"
+  },
+  {
+    "revision": "284ca80cb8d781006c88",
+    "url": "./static/js/main.33fcaba0.chunk.js"
+  },
+  {
+    "revision": "7f50e854530ba03bc381",
+    "url": "./static/js/runtime-main.ce97b5a1.js"
+  },
+  {
+    "revision": "745659340079ec142c9fc28f4c067348",
+    "url": "./static/media/BandBadge.74565934.svg"
+  },
+  {
+    "revision": "dd8acf978709ceb07b1d527e2316d92d",
+    "url": "./static/media/ChainlinkBadge.dd8acf97.svg"
+  },
+  {
+    "revision": "ccbea8504b22e244cbcd6d2868ed5d7e",
+    "url": "./static/media/ELASTOSBadge.ccbea850.svg"
+  },
+  {
+    "revision": "441cf5445197dacf5f15018d4a10c844",
+    "url": "./static/media/MKRBadge.441cf544.png"
+  },
+  {
+    "revision": "2b364873cd68d9283f4d79d5bdd03c09",
+    "url": "./static/media/RSKBadge.2b364873.svg"
+  },
+  {
+    "revision": "d1eabcbb74620a4fb24102ce7cb4c630",
+    "url": "./static/media/TellorBadge.d1eabcbb.svg"
+  },
+  {
+    "revision": "0c528db650525e119501582e05e0be07",
+    "url": "./static/media/heart_empty.0c528db6.svg"
+  },
+  {
+    "revision": "1abb42ad39949d1e1f249a6c40dd33aa",
+    "url": "./static/media/heart_filled.1abb42ad.svg"
+  },
+  {
+    "revision": "bdfd309a832b3616d2e67fedf19d314e",
+    "url": "./static/media/triangledown.bdfd309a.svg"
+  },
+  {
+    "revision": "346229753507e75ce4e5f0e4ddfc0f57",
+    "url": "./static/media/triangleup.34622975.svg"
+  },
+  {
+    "revision": "f242a3ebe7234d83b95e9b10cc3688a7",
+    "url": "./static/media/vulcanwhite.f242a3eb.png"
+  },
+  {
+    "revision": "fbe80a34c0bb69aa82f072d444608c0b",
+    "url": "./static/media/vulcanwhite.fbe80a34.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a710a099f96d4acc855b6c92eb5efae5.js"
+  "./precache-manifest.43ef8d2dd41bba14b8235081cffecfe5.js"
 );
 
 self.addEventListener('message', (event) => {
