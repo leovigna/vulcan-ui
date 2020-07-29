03f48e52b4a1806321022fd9d5b884b6cdf599c8
Leo Vigna <leo@vulcan.link>
2020-07-29 19:11:17 +0200

    Add ELASTOS/RSK/BAND as coming soon

--- src/containers/VulcanLayout/VulcanHeader.tsx
+++ src/containers/VulcanLayout/VulcanHeader.tsx
@@ -1,24 +1,17 @@
 /* eslint @typescript-eslint/explicit-function-return-type:0 */
 
 import React, { Component } from 'react';
-import NetworkPicker from '../../components/NetworkPicker'
-import vulcan from "../../assets/img/brand/vulcanwhite.png"
-import triangledown from "../../assets/img/triangledown.svg"
-import externalLink from "../../assets/img/icons/external_link.png"
+import vulcan from '../../assets/img/brand/vulcanwhite.png';
+import triangledown from '../../assets/img/triangledown.svg';
+import externalLink from '../../assets/img/icons/external_link.png';
 
-import {
-    CNav as Nav,
-    CCol as Col,
-    CRow as Row,
-    CContainer as Container
-} from '@coreui/react';
-
+import { CNav as Nav, CCol as Col, CRow as Row, CContainer as Container } from '@coreui/react';
 
 class DefaultHeader extends Component {
     render() {
         return (
             <>
-                <div style={{ color: '#FFFFFF', backgroundColor: '#002C69' }} >
+                <div style={{ color: '#FFFFFF', backgroundColor: '#002C69' }}>
                     <Container>
                         <Row>
                             <Col xs={12} sm={10} md={10} lg={10}>
@@ -26,19 +19,34 @@ class DefaultHeader extends Component {
                                     <a className="navbar-brand" href="#">
                                         <img src={vulcan} alt="" />
                                     </a>
-                                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                                        aria-label="Toggle navigation">
+                                    <button
+                                        className="navbar-toggler"
+                                        type="button"
+                                        data-toggle="collapse"
+                                        data-target="#navbarResponsive"
+                                        aria-controls="navbarResponsive"
+                                        aria-expanded="false"
+                                        aria-label="Toggle navigation"
+                                    >
                                         <span className="navbar-toggler-icon"></span>
                                     </button>
                                     <div className="collapse navbar-collapse" id="navbarResponsive">
                                         <ul className="navbar-nav ml-auto">
                                             <li className="nav-item">
-                                                <a className="nav-link" href="#">Feeds</a>
+                                                <a className="nav-link" href="#">
+                                                    Feeds
+                                                </a>
                                             </li>
                                             <li className="nav-item">
-                                                <a className="nav-link" href={process.env.REACT_APP_HOMEPAGE_URL} target="_blank">Vulcan.Link<img
-                                                    src={externalLink} /></a>
+                                                <a
+                                                    className="nav-link"
+                                                    href={process.env.REACT_APP_HOMEPAGE_URL}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    Vulcan.Link
+                                                    <img src={externalLink} />
+                                                </a>
                                             </li>
                                             {/*
                                             <li className="nav-item">
@@ -46,26 +54,36 @@ class DefaultHeader extends Component {
                                         <img src={externalLink} /></a>
                                             </li> */}
                                         </ul>
-                                        <NetworkPicker />
+                                        {/* <NetworkPicker /> */}
                                         <form className="form-inline my-lg-0">
-                                            <a href={process.env.REACT_APP_CONTACT_URL} className="btn gettouch my-sm-0" type="submit"
-                                                target="_blank">Contact
-                                    Us</a>
+                                            <a
+                                                href={process.env.REACT_APP_CONTACT_URL}
+                                                className="btn gettouch my-sm-0"
+                                                type="submit"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                Contact Us
+                                            </a>
                                         </form>
-
                                     </div>
                                 </Nav>
                             </Col>
                         </Row>
-                        <Row className='py-3'>
-                            <Col xs='12'>
-                                Explore decentralized and decentralized oracle protocols on the Ethereum blockchain. Oracle feeds enable connecting smart contracts with external data that powers various DeFi protocols.<br />
-                                <br />Keep an eye out for new feeds, oracle protocols and blockchain networks that frequently get added!
+                        <Row className="py-3">
+                            <Col xs="12">
+                                Explore decentralized and decentralized oracle protocols on the Ethereum blockchain.
+                                Oracle feeds enable connecting smart contracts with external data that powers various
+                                DeFi protocols.
+                                <br />
+                                <br />
+                                Keep an eye out for new feeds, oracle protocols and blockchain networks that frequently
+                                get added!
                             </Col>
                         </Row>
                     </Container>
                 </div>
-                <div style={{ height: 0, position: 'relative', backgroundColor: '#002C69' }} >
+                <div style={{ height: 0, position: 'relative', backgroundColor: '#002C69' }}>
                     <img style={{ width: '100%', height: 200 }} src={triangledown}></img>
                 </div>
             </>
--- src/data/protocols.ts
+++ src/data/protocols.ts
@@ -1,9 +1,13 @@
 //@ts-nocheck
-import ChainlinkBadge from '../assets/img/protocols/ChainlinkBadge.svg'
-import TellorBadge from '../assets/img/protocols/TellorBadge.svg'
-import CoinbaseBadge from '../assets/img/protocols/CoinbaseBadge.png'
-import MKRBadge from '../assets/img/protocols/MKRBadge.png'
-import { ProtocolTypes } from '../store/types'
+import ChainlinkBadge from '../assets/img/protocols/ChainlinkBadge.svg';
+import TellorBadge from '../assets/img/protocols/TellorBadge.svg';
+import CoinbaseBadge from '../assets/img/protocols/CoinbaseBadge.png';
+import MKRBadge from '../assets/img/protocols/MKRBadge.png';
+import RSKBadge from '../assets/img/protocols/RSKBadge.svg';
+import ELASTOSBadge from '../assets/img/protocols/ELASTOSBadge.svg';
+import BandBadge from '../assets/img/protocols/BandBadge.svg';
+
+import { ProtocolTypes } from '../store/types';
 
 const protocols: [ProtocolTypes.Protocol] = [
     {
@@ -13,7 +17,8 @@ const protocols: [ProtocolTypes.Protocol] = [
         url: 'https://chain.link',
         img: ChainlinkBadge,
         description: 'Decentralized Oracle Network using LINK token.',
-        descriptionLong: 'The Chainlink Network provides the largest collection of secure and decentralized on-chain price reference data available. Composed of security reviewed, sybil resistant and fully independent nodes which are run by leading blockchain devops and security teams. Creating a shared global resource which is sponsored by a growing list of top DeFi Dapps.',
+        descriptionLong:
+            'The Chainlink Network provides the largest collection of secure and decentralized on-chain price reference data available. Composed of security reviewed, sybil resistant and fully independent nodes which are run by leading blockchain devops and security teams. Creating a shared global resource which is sponsored by a growing list of top DeFi Dapps.',
     },
     {
         id: 'tellor',
@@ -23,7 +28,6 @@ const protocols: [ProtocolTypes.Protocol] = [
         img: TellorBadge,
         description: 'Decentralized Oracle Network combining Proof-of-Work and TRB token.',
         descriptionLong: 'Decentralized Oracle Network combining Proof-of-Work and TRB token.',
-
     },
     {
         id: 'coinbase',
@@ -33,17 +37,43 @@ const protocols: [ProtocolTypes.Protocol] = [
         img: CoinbaseBadge,
         description: 'On-chain market data signed by Coinbase.',
         descriptionLong: 'On-chain market data signed by Coinbase.',
-
     },
     {
         id: 'mkrdao',
         active: true,
         name: 'MKRDao',
-        url: 'https://developer.makerdao.com/feeds/',
+        url: 'https://developer.makerdao.com/feeds',
         img: MKRBadge,
         description: 'Anonymous data feeds backing the DAI stablecoin.',
         descriptionLong: 'Anonymous data feeds backing the DAI stablecoin.',
-    }
-]
+    },
+    ,
+    {
+        id: 'rsk',
+        active: true,
+        name: 'RSK',
+        url: 'https://rsk.co',
+        img: RSKBadge,
+        description: 'Coming soon...',
+    },
+    ,
+    {
+        id: 'elastos',
+        active: true,
+        name: 'ELASTOS',
+        url: 'https://www.elastos.org',
+        img: ELASTOSBadge,
+        description: 'Coming soon...',
+    },
+    ,
+    {
+        id: 'band',
+        active: true,
+        name: 'Band',
+        url: 'https://bandprotocol.com',
+        img: BandBadge,
+        description: 'Coming soon...',
+    },
+];
 
 export default protocols;
